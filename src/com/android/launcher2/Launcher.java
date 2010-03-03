59222cf2e8d04891447ad3b4449dd3c6bf1ba331
Omprakash Dhyade c_odhyad@quicinc.com <c_odhyad@quicinc.com>
2010-03-03 14:05:05 -0800

    pacakges/app: Use the stored app icon memory only when orientation changes
    Use the stored app icon memory only when orientation changes.

--- src/com/android/launcher2/Launcher.java
+++ src/com/android/launcher2/Launcher.java
@@ -159,6 +159,7 @@ public final class Launcher extends Activity
     private static final String RUNTIME_INITIAL_CREATION = "launcher.initial_creation";
 
     private boolean initialCreation = true;
+    private int currentOrientation;
 
     static final int APPWIDGET_HOST_ID = 1024;
 
@@ -287,6 +288,7 @@ public final class Launcher extends Activity
             writeConfiguration(this, localeConfiguration);
             mIconCache.flush();
         }
+	currentOrientation = getWindowManager().getDefaultDisplay().getOrientation();
     }
 
     private static class LocaleConfiguration {
@@ -925,7 +927,8 @@ public final class Launcher extends Activity
             outState.putLong(RUNTIME_STATE_PENDING_FOLDER_RENAME_ID, mFolderInfo.id);
         }
 
-	outState.putBoolean(RUNTIME_INITIAL_CREATION, false);
+	if (currentOrientation != getWindowManager().getDefaultDisplay().getOrientation())
+	    outState.putBoolean(RUNTIME_INITIAL_CREATION, false);
     }
 
     @Override
